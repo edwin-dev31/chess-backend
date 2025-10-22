c6f2c53984ab44cdd8f1c8013c1f46caef114300
edwin-dev31 <edwin_dev@hotmail.com>
2025-10-21 23:46:55 -0500

    Configure project for Railway deployment

--- src/main/java/com/chess/game/util/AppRoutes.java
+++ src/main/java/com/chess/game/util/AppRoutes.java
@@ -2,5 +2,5 @@ package com.chess.game.util;
 
 public class AppRoutes {
     public static String FRONTEND_BASE_URL = "https://edwin-dev31.github.io/chess-frontend";
-    public static final String FRONTEND_REDIRECTION_URL = FRONTEND_BASE_URL + "/oauth2-success?token=";
+    public static final String FRONTEND_REDIRECTION_URL = FRONTEND_BASE_URL + "/#/oauth2-success?token=";
 }
